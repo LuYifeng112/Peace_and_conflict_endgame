91b0ecf81b92233bf4f014e8aceed07464da0ce6
LuYifeng112 <45448703+LuYifeng112@users.noreply.github.com>
2019-06-18 08:04:36 +1200

    Brunei, Singapore, North Korea
    state fixes, ideology fixes, leader fixes included. Some graphical changes in texture and colour also

---
+++ mod/common/defines/IC_Graphics_Defines -1.lua
@@ -0,0 +1,9 @@
+NDefines_Graphics.NGraphics.VICTORY_POINT_MAP_ICON_AFTER = {0, 10} -- After this amount of VP the map icon becomes bigger dot.
+NDefines_Graphics.NFrontend.CAMERA_MAX_HEIGHT = 4000.0
+NDefines_Graphics.NFrontend.CAMERA_MIN_HEIGHT = 30.0
+NDefines_Graphics.NMapIcons.STATES_PRIORITY_VICTORY_POINTS = 4
+NDefines_Graphics.NGraphics.COUNTRY_COLOR_HUE_MODIFIER = 0.0 -- 0.0
+NDefines_Graphics.NGraphics.COUNTRY_COLOR_SATURATION_MODIFIER = 0.7 -- 0.6
+NDefines_Graphics.NGraphics.COUNTRY_COLOR_BRIGHTNESS_MODIFIER = 0.8 -- 0.8
+NDefines_Graphics.NGraphics.COUNTRY_FLAG_STRIPE_TEX_MAX_HEIGHT = 1750
+NDefines_Graphics.NGraphics.COUNTRY_FLAG_LARGE_STRIPE_MAX_HEIGHT = 6500
